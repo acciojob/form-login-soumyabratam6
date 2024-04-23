3609a80ce717e801d2ac718344307ed9014d9a54
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-04-23 21:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	var fastname = document.getElementById("Fname").value;
+	var lastname = document.getElementById("Lname").value;
+	 var fullName = fastname + " " + lastname;
+	alert(fullName);
 
 }
